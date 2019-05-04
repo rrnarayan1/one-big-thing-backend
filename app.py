95a10e37d5e4cffcada3e733ddc3f130c6341b83
Rohan <rrnarayan1@gmail.com>
2019-05-04 13:57:20 -0700

    more changes to app server, added compatibility for getting data prior to that game

--- app.py
+++ app.py
@@ -29,9 +29,9 @@ def hello():
 
 @app.route('/game')
 def game():
-    season_id = request.args.get('seasonId')
-    team_id = request.args.get('teamId')
-    game_id = request.args.get('gameId')
+    season_id = request.form.get('seasonId')
+    team_id = request.form.get('teamId')
+    game_id = request.form.get('gameId')
     if (not season_id or not team_id or not game_id):
         return flask.abort(400)
 
@@ -42,23 +42,49 @@ def game():
 
 @app.route('/score')
 def score():
-    season_id = request.args.get('seasonId')
-    team_id = request.args.get('teamId')
-    game_id = request.args.get('gameId')
-    data = request.args.get('data')
-    data_type = request.args.get('dataType')
-    if (not season_id or not team_id or not game_id or not data):
+    season_id = request.form.get('seasonId')
+    team_id = request.form.get('teamId')
+    game_id = request.form.get('gameId')
+    data = request.form.get('data')
+    portion = request.form.get('portion')
+
+    if (not season_id or not team_id or not game_id):
         return flask.abort(400)
 
     game = _get_game(season_id, team_id, game_id)
     if (not game):
         return flask.abort(404)
 
-    # assume the data is for the whole season
+    # if data not defined, provide a default
+    if (not data):
+        data_season_id = int(game[u"SEASON_ID"])
+        # if playoffs, use the regular season
+        if (data_season_id > 40000):
+            data_season_id -= 20000
+            data = str(data_season_id)
+
+        # if at beginning of season, use last season as data
+        elif (game["GAME_NUM"] < 10):
+            data = str(data_season_id-1)
+        
+        # else, take that season's games up to that point
+        else:
+            data = str(data_season_id)
+            portion = "before"
+
+    # only use portion = before if data is for that year
+    if (portion == "before" and game[u"SEASON_ID"] != data):
+        return flask.abort(422)
+
     data_season_collection = seasons[data]
-    team_unicode = (team_id).encode("utf-8")
-    data_games = db.collection(data_season_collection) \
-        .where(u"TEAM_ID", u"==",team_id).get()
+    if (portion == "before"):
+        data_games = db.collection(data_season_collection) \
+            .where(u"TEAM_ID", u"==", team_id) \
+            .where(u"GAME_NUM", u"<", game[u"GAME_NUM"]).get()
+    else:
+        data_games = db.collection(data_season_collection) \
+            .where(u"TEAM_ID", u"==",team_id).get()
+
     if (not data_games):
         return flask.abort(404)
 
@@ -66,17 +92,21 @@ def score():
     for data_game in data_games:
         list_data_games.append(data_game.to_dict())
 
+    response = {}
     game_stat = pd.Series(game)[stat_categories]
     df = pd.DataFrame(list_data_games)
     df = df[stat_categories]
     mean = df.mean()
     std = df.std()
     diff = (game_stat - mean) / std
-    diff_dict = diff.to_dict()
-    print(diff_dict)
-    return flask.jsonify(diff_dict)
 
-# returns the game as a dictionary
+    response["data"] = data;
+    response["portion"] = portion;
+    response["scores"]=diff.to_dict()
+    response["game"]=game
+    return flask.jsonify(response)
+
+# returns the game as a dictionary. if not present, returns None
 def _get_game(season_id, team_id, game_id):
     season_collection = seasons[season_id]
     doc_ref = db.document(season_collection,team_id + "-" + game_id)
